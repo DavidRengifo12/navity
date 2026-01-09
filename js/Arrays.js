78f086cbad8d6991d3bd47dc5b6409a9fe6a2526
DavidRengifo12 <davidsantiagorengifoguacheta@gmail.com>
2026-01-09 17:15:21 -0500

    ARRAYS REPASOS

---
+++ js/Arrays.js
@@ -0,0 +1,11 @@
+let notas = [2.5, 3.0, 4.2, 1.8, 5.0, 2.9]
+
+let aprobados = 0
+
+for(let i = 0; i <= notas.length; i++){
+    if(notas[i] >=3 ){
+        aprobados ++
+    }
+}
+
+console.log("Cantidad de aprobados:", aprobados)
