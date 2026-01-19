ba933ecddcf684dad802c23321c5c6ff156203ea
DavidRengifo12 <davidsantiagorengifoguacheta@gmail.com>
2026-01-19 16:53:05 -0500

    practices

--- js/Arrays.js
+++ js/Arrays.js
@@ -1,4 +1,4 @@
-let notas = [2.5, 3.0, 4.2, 1.8, 5.0, 2.9]
+/*let notas = [2.5, 3.0, 4.2, 1.8, 5.0, 2.9]
 
 let aprobados = 0
 
@@ -9,3 +9,9 @@ for(let i = 3; i <= notas.length; i++){
 }
 
 console.log("Cantidad de aprobados:", aprobados)
+
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for(let x = 0; x < numbers.length; x++){
+    console.log('Numeros elementos del array son: ', numbers[x])
+}*/
