34456dc290757954e9778f02c9a60702ac707314
DavidRengifo12 <davidsantiagorengifoguacheta@gmail.com>
2026-01-20 16:32:16 -0500

    practicess

--- js/Fuctions.js
+++ js/Fuctions.js
@@ -11,5 +11,17 @@ let numbers = [1, 9]
 console.log('Suma del array es:',sumaArray(numbers))*/
 
 
+const DevolverPromedio = (promedios) => {
+    let suma = 0
 
+    for(let b = 0; b < promedios.length; b++){
+        suma += promedios[b]
+    }
+    let promediar = suma / promedios.length
+    return promediar
+    
+}
+
+let notas = [2.5, 3.5, 4.5, 5.0]
+console.log(DevolverPromedio(notas))
 
