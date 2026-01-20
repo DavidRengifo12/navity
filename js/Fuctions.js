1e773adf35f97c5dda5e512819776a64fbe91b2b
DavidRengifo12 <davidsantiagorengifoguacheta@gmail.com>
2026-01-20 16:19:23 -0500

    practicess

---
+++ js/Fuctions.js
@@ -0,0 +1,15 @@
+
+/*const sumaArray = (arraySumar) => {
+    let sumaDelArray = 0
+    for(let a = 0; a < arraySumar.length; a++){
+        sumaDelArray += arraySumar[a]
+    }
+    return sumaDelArray
+} 
+let numbers = [1, 9]
+
+console.log('Suma del array es:',sumaArray(numbers))*/
+
+
+
+
